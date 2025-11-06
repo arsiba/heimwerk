a5e3a8b182678e48487e74efd7596e5caeb6ff57
Arne Bauer <arne@bauers.online>
2025-11-06 18:22:28 +0100

    feat: create user model

--- serviceCatalog/apps.py
+++ serviceCatalog/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ServicecatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'serviceCatalog'
+
+    def ready(self):
+        import serviceCatalog.signals
