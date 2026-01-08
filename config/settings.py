4958d228528ed4ff57df6423b80d14f4c10d821f
Arne Bauer <arne@bauers.online>
2026-01-08 11:54:34 +0100

    fix: try to fix csrf problems

--- config/settings.py
+++ config/settings.py
@@ -170,6 +170,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    "CSRF_TRUSTED_ORIGINS", "http://localhost http://127.0.0.1"
-).split()
+raw_origins = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost http://127.0.0.1")
+CSRF_TRUSTED_ORIGINS = raw_origins.replace('"', "").replace("'", "").split()
